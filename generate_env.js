c66c7162fc0eddda81fa7275bd98e1f6c667fc92
Viktoria <v.s.polikanova@gmail.com>
2025-03-03 14:39:24 +0700

    fix generate env

--- generate_env.js
+++ generate_env.js
@@ -4,6 +4,6 @@ const apiUrl = process.env.API_URL || "https://default-api-url.com";
 
 const envContent = `window.env = { API_URL: "${apiUrl}" };`;
 
-fs.writeFileSync("frontend/env.js", envContent, { encoding: "utf8" });
+fs.writeFileSync("env.js", envContent, { encoding: "utf8" });
 
 console.log(`✅ env.js сгенерирован: API_URL = ${apiUrl}`);
